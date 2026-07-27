46c4c90570b81fdf80f446574dc68d26ad2a1fa2
F1A-M0ND <dddbbb234455@gmail.com>
2026-07-27 21:34:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+    base: "./",
+
     server: {
         cors: {
             origin: "https://www.owlbear.rodeo",
